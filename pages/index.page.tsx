22ea4244ac76daa9e4cb76a7e993d9c51d61e296
Sai Krishna <skris4291@gmail.com>
2021-07-02 21:12:16 +0530

    feat: add custom home page

--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -1,8 +1,46 @@
-import { Button, Heading } from '@chakra-ui/react';
+import { Button, Flex, Heading, Link, Text } from '@chakra-ui/react';
+import styled from '@emotion/styled';
 import { useSession } from 'next-auth/client';
 import React from 'react';
 
 export { Page };
+
+const modules = [
+  'ViteJS',
+  'Vite-Plugin-SSR',
+  'TypeScript',
+  'React',
+  'Next-Auth',
+  'Chakra-UI',
+  'ESLint',
+  'Prettier',
+  'Husky',
+  'Commitlint',
+];
+
+const Root = styled(Flex)`
+  width: 100vw;
+  height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+`;
+
+const GradText = styled(Heading)`
+  background-color: #f3ec78;
+  background-image: linear-gradient(
+    45deg,
+    rgba(62, 161, 219, 1) 11.2%,
+    rgba(93, 52, 236, 1) 100.2%
+  );
+  background-size: 100%;
+  background-clip: text;
+  -webkit-background-clip: text;
+  -moz-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  -moz-text-fill-color: transparent;
+`;
 
 function Page() {
   const [session, loading] = useSession();
@@ -12,21 +50,31 @@ function Page() {
 
   if (session) {
     return (
-      <>
-        <Heading>Welcome {session.user?.name}</Heading>
-        <a href="/api/auth/signout">
-          <Button>Sign out</Button>
-        </a>
-      </>
+      <Root>
+        <GradText fontSize="8xl">Welcome {session.user?.name}</GradText>
+        <Link href="/api/auth/signout" _hover={{ textDecor: 'none' }}>
+          <Button colorScheme="purple" mt={10}>
+            Sign out
+          </Button>
+        </Link>
+        <Text mt={5}>Your details are not stored on our server</Text>
+      </Root>
     );
   }
 
   return (
-    <>
-      <Heading>Who are you?</Heading>
-      <a href="/api/auth/signin">
-        <Button>Sign in</Button>
-      </a>
-    </>
+    <Root>
+      <GradText fontSize="7xl">Vite SSR Starter</GradText>
+      <Flex flexWrap="wrap" p={5} mt={5} mb={10} textAlign="center">
+        {modules.map((m) => (
+          <Text key={m} p={1}>
+            {m}
+          </Text>
+        ))}
+      </Flex>
+      <Link href="/api/auth/signin" _hover={{ textDecor: 'none' }}>
+        <Button colorScheme="purple">Test Next-Auth</Button>
+      </Link>
+    </Root>
   );
 }
